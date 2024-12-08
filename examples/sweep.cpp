df3d13cbaf84144e7f0f9f750e77e5c83f2166c9
Kyle Yang <REmpire.dev@proton.me>
2024-12-08 15:03:01 -0500

    added example to folder

---
+++ examples/sweep.cpp
@@ -0,0 +1,24 @@
+#include "Adafruit_PWMServoDriver.h"
+
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+auto main() -> int {
+
+    Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+    pwm.setPWMFreq(60.0);
+
+    while (true) {
+        pwm.setPWM(0, 0, 370);
+        usleep(1'000'000);
+        pwm.setPWM(0, 0, 415);
+        usleep(1'000'000);
+        pwm.setPWM(0, 0, 460);
+        usleep(1'000'000);
+        pwm.setPWM(0, 0, 415);
+        usleep(1'000'000);
+    }
+}
